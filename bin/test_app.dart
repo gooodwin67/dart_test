8775c0d60d361985604ca6e3d00e3adc43b2fbf7
Andrey <sn67@inbox.ru>
2022-01-26 14:47:35 +0300

    test

--- bin/test_app.dart
+++ bin/test_app.dart
@@ -1,12 +1,14 @@
+import 'dart:convert';
 import 'dart:io';
+import 'package:http/http.dart' as http;
 
 void main() async {
   print('start');
-  printFile;
+  //printFile;
   /*  var g580 = Keyboards('g580');
   print(g580); */
-
-  await printNum;
+  //await printNum;
+  printUsers();
 
   print('end');
 }
@@ -32,3 +34,16 @@ Future printFile = Future(() {
       new File('C:/Dart/test/test_app/bin/test.txt').readAsStringSync();
   return myFile;
 }).then((value) => print(value));
+
+printUsers() {
+  http
+      .get(Uri.parse(('https://jsonplaceholder.typicode.com/users/1')))
+      .then((value) {
+    try {
+      var users2 = json.decode(value.body);
+      print(users2);
+    } catch (e) {
+      print(e);
+    }
+  });
+}
